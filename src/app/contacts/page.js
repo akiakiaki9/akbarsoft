9c357566ec03f7bea6976470bdbc2170fd60dd6a
Akbar <tillayevakbarshoh@gmail.com>
2025-02-17 22:06:31 +0500

    akbarsoftv1

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react'
 import Contacts from './Contacts'
 import Footer from '../components/footer/Footer'
--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react'
 import Services from './Services'
 import Navbar from '../components/navbar/Navbar'
