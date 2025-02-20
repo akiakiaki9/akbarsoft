dacdee304b7def00bd529417e149a741458f1f4b
Akbar <tillayevakbarshoh@gmail.com>
2025-02-20 10:57:47 +0500

    gg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: ["akbarsoft.uz"],
+    },
+};
 
 export default nextConfig;
