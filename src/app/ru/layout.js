03545bd9202731a9ec01b0f0d20bb07ae1884890
Akbar <tillayevakbarshoh@gmail.com>
2025-05-15 16:18:16 +0500

    d

--- src/app/ru/layout.js
+++ src/app/ru/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta name="yandex-verification" content="f201683b2288240d" />
